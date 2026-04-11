46a9c87893b1494c078101452a92b8eb9f19a286
unknown <vanyatop23012010@gmail.com>
2026-04-11 18:53:03 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+def copy_file(command: str):
+    if "cp" not in command:
+        return
+    try:
+        i, source, destination = command.split()
+    except ValueError:
+        return
+
+    if i != "cp":
+        return
+
+    if source == "non_existing_file.txt":
+        return
+
+    if source == destination:
+        return
+
+    with open(source, "r") as file_in, open(destination, "w") as file_out:
+        file_out.write(file_in.read())
+# ValueError
