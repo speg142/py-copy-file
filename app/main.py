42765f812c047bfe121961e4f922ce6b512738f3
unknown <vanyatop23012010@gmail.com>
2026-04-11 19:14:27 +0300

    Solution4

--- app/main.py
+++ app/main.py
@@ -10,8 +10,5 @@ def copy_file(command: str) -> None:
     if source == destination:
         return
 
-    try:
-        with open(source, "r") as file_in, open(destination, "w") as file_out:
-            file_out.write(file_in.read())
-    except FileNotFoundError:
-        return
+    with open(source, "r") as file_in, open(destination, "w") as file_out:
+        file_out.write(file_in.read())
