81e198179abd7c93f098be5c77589411c56513d2
unknown <vanyatop23012010@gmail.com>
2026-04-17 19:29:16 +0300

    Solution5

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 def copy_file(command: str) -> None:
     try:
-        cmd, source, destination = command.split()
+        command_name, source, destination = command.split()
     except ValueError:
         return
 
-    if cmd != "cp":
+    if command_name != "cp":
         return
 
     if source == destination:
         return
 
-    with open(source, "r") as file_in, open(destination, "w") as file_out:
-        file_out.write(file_in.read())
+    try:
+        with open(source, "r") as file_in, open(destination, "w") as file_out:
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        return
